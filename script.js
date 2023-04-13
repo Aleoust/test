88a4ca708bf0f7ef4893d8b9cf016972139b5d65
Alexis <130687041+Aleoust@users.noreply.github.com>
2023-04-13 18:40:27 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function foncJaune() {
+    document.querySelector(".returnhome").classList.add("jaune");
+    document.querySelector(".returnhome").classList.remove("noir");
+}
+function foncNoir() {
+	document.querySelector(".returnhome").classList.remove("jaune");
+	document.querySelector(".returnhome").classList.add("noir");
+}
